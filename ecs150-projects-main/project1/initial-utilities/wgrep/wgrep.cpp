0d7c00ad2e70bdb4e3c662bef47068cddf36b1ed
Noboru Tokimi <tkmnbrgit@gmail.com>
2024-10-02 12:31:56 -0700

    Refactor wgrep.cpp to improve code readability and maintainability

--- ecs150-projects-main/project1/initial-utilities/wgrep/wgrep.cpp
+++ ecs150-projects-main/project1/initial-utilities/wgrep/wgrep.cpp
@@ -31,70 +31,46 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-    int fileDescriptor;
-    if(argc < 2){
+void grepFromFileDescriptor(int fileDescriptor, const char *searchTerm) {
+    char buffer[4096];
+    int ret;
+    std::string line;
+    while ((ret = read(fileDescriptor, buffer, sizeof(buffer))) > 0) {
+        for (int i = 0; i < ret; i++) {
+            if (buffer[i] == '\n') {
+                line += buffer[i];
+                if (line.find(searchTerm) != std::string::npos) {
+                    write(STDOUT_FILENO, line.c_str(), line.size());
+                }
+                line.clear();
+            } else {
+                line += buffer[i];
+            }
+        }
+    }
+    if (!line.empty() && line.find(searchTerm) != std::string::npos) {
+        write(STDOUT_FILENO, line.c_str(), line.size());
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
         write(STDOUT_FILENO, "wgrep: searchterm [file ...]\n", 29);
         return 1;
     }
     const char *searchTerm = argv[1];
-    if(argc == 2){
-        fileDescriptor = STDIN_FILENO;
-        char buffer[4096];
-        int ret;
-        std::string line;
-        while((ret = read(fileDescriptor, buffer, sizeof(buffer))) > 0){
-                for(int i = 0; i < ret; i++){
-                    if(buffer[i] == '\n'){
-                        line += buffer[i];
-                        if(line.find(searchTerm) != std::string::npos){
-                            write(STDOUT_FILENO, line.c_str(), line.size());
-                        }
-                        line.clear();
-                    } else {
-                        line += buffer[i];
-                    }
-                }
-            }
-            if(!line.empty() && line.find(searchTerm) != std::string::npos){
-                write(STDOUT_FILENO, line.c_str(), line.size());
-            }
-            if(fileDescriptor != STDIN_FILENO){
-                close(fileDescriptor);
-            }
-    }
-    else{
-        for(int i = 2; i < argc; i++){
-            fileDescriptor = open(argv[i], O_RDONLY);
-            if(fileDescriptor == -1){
+    if (argc == 2) {
+        grepFromFileDescriptor(STDIN_FILENO, searchTerm);
+    } else {
+        for (int i = 2; i < argc; i++) {
+            int fileDescriptor = open(argv[i], O_RDONLY);
+            if (fileDescriptor == -1) {
                 write(STDOUT_FILENO, "wgrep: cannot open file\n", 24);
                 return 1;
             }
-            char buffer[4096];
-            int ret;
-            std::string line;
-            // if there is a search term, but no specified file, read from standard input
-
-
-            while((ret = read(fileDescriptor, buffer, sizeof(buffer))) > 0){
-                for(int i = 0; i < ret; i++){
-                    if(buffer[i] == '\n'){
-                        line += buffer[i];
-                        if(line.find(searchTerm) != std::string::npos){
-                            write(STDOUT_FILENO, line.c_str(), line.size());
-                        }
-                        line.clear();
-                    } else {
-                        line += buffer[i];
-                    }
-                }
-            }
-            if(!line.empty() && line.find(searchTerm) != std::string::npos){
-                write(STDOUT_FILENO, line.c_str(), line.size());
-            }
-            if(fileDescriptor != STDIN_FILENO){
-                close(fileDescriptor);
-            }
+            grepFromFileDescriptor(fileDescriptor, searchTerm);
+            close(fileDescriptor);
         }
     }
+    return 0;
 }
